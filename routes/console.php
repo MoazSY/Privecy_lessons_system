a6e9d11d8ba981d21a65398ca1278bfd6fb3a643
Moazmar <moazmar2003@gmail.com>
2025-08-26 21:54:57 +0300

    sheduler

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,11 @@ Schedule::command('zoom:sessions:auto-create')->everyMinute()->appendOutputTo(st
 Schedule::command('zoom:sessions:auto-end')->everyMinute()->appendOutputTo(storage_path('logs/zoom-auto-end.log'));
 
 
+Artisan::command('schedule:run-continuously', function () {
+    $this->info('Starting continuous scheduler...');
+
+    while (true) {
+        Artisan::call('schedule:run');
+        sleep(60);
+     }
+})->purpose('Run scheduler continuously without cron');
